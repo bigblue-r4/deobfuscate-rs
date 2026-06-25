23f140547645e6d2d60493c439316877e20f96d0
Christopher Howard <trentdoosday@gmail.com>
2026-06-24 21:28:59 -1000

    chore: add .gitignore, exclude target/

--- target/debug/examples/basic-0a41021366bcb4c3.d
+++
@@ -1,7 +0,0 @@
-/home/evillab/Desktop/deobfuscate-rs/target/debug/examples/basic-0a41021366bcb4c3.d: examples/basic.rs
-
-/home/evillab/Desktop/deobfuscate-rs/target/debug/examples/basic-0a41021366bcb4c3: examples/basic.rs
-
-examples/basic.rs:
-
-# env-dep:CARGO_PKG_VERSION=1.0.0
--- target/debug/examples/basic.d
+++
@@ -1,1 +0,0 @@
-/home/evillab/Desktop/deobfuscate-rs/target/debug/examples/basic: /home/evillab/Desktop/deobfuscate-rs/examples/basic.rs /home/evillab/Desktop/deobfuscate-rs/src/lib.rs
